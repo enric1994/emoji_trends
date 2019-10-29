bbd83e6ee546858db08c84ddab0d30c8c3c30bff
Enric Moreu <enric.moreu.filella@gmail.com>
2019-10-29 19:05:57 +0000

    Full pipeline ready

--- get_emojis.py
+++ get_emojis.py
@@ -3,39 +3,79 @@ import datetime
 import psutil
 import time
 
+# Stop all: sudo pkill python
+# Check workers: ps -fC python3 |wc -l
+
+MAX_LOAD_AVG = 4
+start_date = datetime.datetime(2010,1,1)
+days_number = 3500
+
+🇯🇵 🇰🇷 🇩🇪 🇨🇳 🇺🇸 🇫🇷 🇪🇸 🇮🇹 🇷🇺 🇬🇧
+
 emojis=[
-    ["football","⚽"]
+    ["football","⚽"],
+    ["bee","🐝"],
+    ["american_football","🏈"],
+    ["uk","🇬🇧"],
+    ["spain","🇪🇸"],
+    ["catalonia", "🎗️"],
+    ["shooting_star","🌠"],
+    ["factory", "🏭"],
+    ["pig", "🐖"],
+    ["panda", "🐼"],
+    ["snake", "🐍"],
+    ["santa","🎅"],
+    ["fuel", "⛽"],
+    ["game","🎮"],
+    ["beer","🍺"],
+    ["chart_incr","📈"],
+    ["chart_decr","📉"],
+    ["japan","🇯🇵"],
+    ["korea","🇰🇷"],
+    ["germany","🇩🇪"],
+    ["china","🇨🇳"],
+    ["france","🇫🇷"],
+    ["itlay","🇮🇹"],
+    ["money","💸"],
+    ["rose", "🌹"],
+    ["recycle","♻️"],
+    ["broken", "💔"],
+    ["angry","😠"]
+    
 ]
 
-os.makedirs('results/'+emojis[0][0], exist_ok=True)
+for emoji in emojis:
+
+    os.makedirs('results/' + emoji[0], exist_ok=True)
 
 
-start_date = datetime.datetime(2018,1,1)
-for i in range(365):
+    for i in range(days_number):
 
-    next_date = start_date + datetime.timedelta(days=i+1)
-    next_year = str(next_date.year).zfill(4) 
-    next_month = str(next_date.month).zfill(2) 
-    next_day = str(next_date.day).zfill(2) 
+        next_date = start_date + datetime.timedelta(days=i+1)
+        next_year = str(next_date.year).zfill(4) 
+        next_month = str(next_date.month).zfill(2) 
+        next_day = str(next_date.day).zfill(2) 
 
-    current_date = start_date + datetime.timedelta(days=i)
-    current_year = str(current_date.year).zfill(4) 
-    current_month = str(current_date.month).zfill(2) 
-    current_day = str(current_date.day).zfill(2) 
-
-
-    lavg = psutil.getloadavg()[0]
-    while lavg >10:
-        time.sleep(60)
-        print('waiting for workers...')
+        current_date = start_date + datetime.timedelta(days=i)
+        current_year = str(current_date.year).zfill(4) 
+        current_month = str(current_date.month).zfill(2) 
+        current_day = str(current_date.day).zfill(2) 
 
 
+        lavg = psutil.getloadavg()[0]
+        while lavg > MAX_LOAD_AVG:
+            print('waiting for workers...')
+            time.sleep(60)
+            
 
-    os.system('python3 Exporter.py --lang "en" --querysearch "{}" --maxtweets 100000000 --output={}.csv --since {}-{}-{} --until {}-{}-{} &'.format(
-        emojis[0][1],
-        'results/{}/{}_{}-{}-{}'.format(emojis[0][0], emojis[0][0], current_year, current_month, current_day),
-        current_year,current_month,current_day,
-        next_year,next_month,next_day
-    ))
 
-    time.sleep(2)
+        print("{} worker {} created".format(emoji[1], i))
+
+        os.system('python3 Exporter.py --lang "en" --querysearch "{}" --maxtweets 100000000 --output={}.csv --since {}-{}-{} --until {}-{}-{} &'.format(
+            emoji[1],
+            'results/{}/{}_{}-{}-{}'.format(emoji[0], emoji[0], current_year, current_month, current_day),
+            current_year,current_month,current_day,
+            next_year,next_month,next_day
+        ))
+
+        time.sleep(2)
