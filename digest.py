f21c63a191aa5a62bc9976d0782adbb996cc9298
Enric Moreu <enric.moreu.filella@gmail.com>
2019-11-01 11:06:34 +0000

    Digest generalization

--- digest.py
+++ digest.py
@@ -1,34 +1,43 @@
 import csv
 import datetime
+import os
 
 start_date = datetime.datetime(2010,1,1)
 days_number = 3500
 date_array = [start_date + datetime.timedelta(days=x) for x in range(days_number)]
-date_dict = {}
 
-for date in date_array:
-    date_dict[date] = 0
+def clean(folder_name):
 
-with open('results/preprocessed/football_clean.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        try:
-            datetime_object = datetime.datetime.strptime(row[1],'%Y-%m-%d %H:%M').replace(hour=0,minute=0)
-            # import pdb;pdb.set_trace()
-            date_dict[datetime_object] += 1
-        except:
-            pass
+    print('Merging all CSV files in one...')
+    os.system('cat results/{}/* > postprocessing/{}_clean.csv'.format(folder_name, folder_name))
+    print('Removing headers...')
+    remove = '"username","date","retweets","favorites","text","geo","mentions","hashtags","id","permalink","emoji"'
+    os.system("awk '!/{}/' postprocessing/{}_clean.csv > temp && mv temp postprocessing/{}_no_header.csv".format(remove, folder_name, folder_name))
 
-        line_count +=1
-        print(line_count)
+    date_dict = {}
 
-print("Writing CSV...")
-with open('output.csv', mode='w') as f:
-    writer = csv.writer(f, delimiter=',')
     for date in date_array:
-        # import pdb;pdb.set_trace()
-        writer.writerow([date.strftime("%Y-%m-%d"), date_dict[date]])
+        date_dict[date] = 0
+
+    print('Digesting...')
+    with open('postprocessing/{}_no_header.csv'.format(folder_name)) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            try:
+                datetime_object = datetime.datetime.strptime(row[1],'%Y-%m-%d %H:%M').replace(hour=0,minute=0)
+                date_dict[datetime_object] += 1
+            except:
+                pass
+
+            line_count +=1
+
+    print("Writing CSV...")
+    with open('output/{}.csv'.format(folder_name), mode='w') as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow(['day,usage'])
+        for date in date_array:
+            writer.writerow([date.strftime("%Y-%m-%d"), date_dict[date]])
 
 
-
+# clean('bee')
