93acd2dfb5d96241bde84bb47c4f4704723d6a85
Enric Moreu <enric.moreu.filella@gmail.com>
2019-11-07 10:19:07 +0000

    Bulk sentiment analysis

--- sentiments.py
+++ sentiments.py
@@ -5,8 +5,8 @@ import os
 from textblob import TextBlob
 
 
-def get_all_dates(name, top_num):
-    with open('output/{}.csv'.format(name), 'r') as f:
+def get_all_dates(name):
+    with open('/media/enric/enric_hdd/datasets/emoji_trends/emojis_50/{}.csv'.format(name), 'r') as f:
         data = []
         dates = []
         response = []
@@ -16,41 +16,60 @@ def get_all_dates(name, top_num):
         for row in csvf:
             data.append(row[1])
             dates.append(row[0])
-        # data = list(map(int, data))
-        # top10index = np.array(data).argsort()[-top_num:]
-        # for i in top10index:
-        #     response.append(dates[i])
-        # print(response[::-1])
         return dates, data
 
 
 def get_tweets_on_date(date, name):
 
-    print('Filtering day...')
-    os.system("awk '/{}/' {} > {}".format(date, 'whathappened/{}_no_header.csv'.format(name), 'whathappened/{}_day.csv'.format(name)))
+    os.system("awk '/{}/' {} > {}".format(date, '/media/enric/enric_hdd/datasets/emoji_trends/clean_emojis/{}_no_header.csv'.format(name), 'tmp/{}_day.csv'.format(name)))
     # Add header
     header='"username","date","retweets","favorites","text","geo","mentions","hashtags","id","permalink","emoji"'
-    os.system("sed -i '1 i\{}' {}".format(header, 'whathappened/{}_day.csv'.format(name)))
+    os.system("sed -i '1 i\{}' {}".format(header, 'tmp/{}_day.csv'.format(name)))
 
-    print('Reading day...')
-    tweets = pd.read_csv('whathappened/{}_day.csv'.format(name))["text"]
-    print('Cleaning temp file...')
-    os.system('rm {}'.format('whathappened/{}_day.csv'.format(name)))
+    tweets = pd.read_csv('tmp/{}_day.csv'.format(name))["text"]
+    os.system('rm {}'.format('tmp/{}_day.csv'.format(name)))
     return tweets
 
-def analyze(date, word):
-    dates, data = get_all_dates(word, 5)
+def analyze(word):
+    print(word)
+    dates, data = get_all_dates(word)
     with open('data/sentiment/{}.csv'.format(word), 'w') as f2:
             csvf2 = csv.writer(f2, delimiter=',', quoting=csv.QUOTE_NONE, escapechar=' ')
             csvf2.writerow(['day','usage'])
             for i, day in enumerate(dates):
                 tweets = get_tweets_on_date(day, word)
                 sentiment = TextBlob(tweets.to_string())
-                print(day)
                 score = str(sentiment.sentiment.polarity*100 + 100)[:3]
-                print(score)
+                if score[-1] == '.':
+                    score=score[:2]+"0"
                 csvf2.writerow([day, str(data[i]) + "." + score])
 
     
 
-analyze('2019-02-04', 'beer2')
+analyze("football")
+analyze("bee")
+analyze("american_football")
+analyze("uk")
+analyze("spain")
+analyze("catalonia")
+analyze("shooting_star")
+analyze("factory")
+analyze("pig")
+analyze("panda")
+analyze("snake")
+analyze("santa")
+analyze("fuel")
+analyze("game")
+analyze("beer")
+analyze("chart_incr")
+analyze("chart_decr")
+analyze("japan")
+analyze("korea")
+analyze("germany")
+analyze("china")
+analyze("france")
+analyze("itlay")
+analyze("money")
+analyze("rose")
+analyze("broken")
+analyze("angry")
